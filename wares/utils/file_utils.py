2dd4cad1134d17e525ba83c2fdc3386a41147cff
Gopal Narayanan <gopastro@gmail.com>
2018-02-10 15:38:05 -0500

    Fix for most recent file

--- wares/utils/file_utils.py
+++ wares/utils/file_utils.py
@@ -1,14 +1,25 @@
 import os
 import glob
 
-def get_nc_file(obsnum, basepat='/data_lmt/spectrometer', roach_id=0):
+def most_recent_file(glob_pattern):
+    return max(glob.iglob(glob_pattern), key=os.path.getctime)
+
+def get_nc_file(obsnum, basepat='/data_lmt/spectrometer', roach_id=0,
+                most_recent=True):
     basepath = os.path.join(basepat, "roach%1d"%  roach_id)
-    fnames = glob.glob(os.path.join(basepath, "roach%1d_%d_*.nc" % (roach_id, obsnum)))
+    glob_pattern = os.path.join(basepath, "roach%1d_%d_*.nc" % (roach_id, obsnum))
+    if most_recent:
+        return most_recent_file(glob_pattern)
+    fnames = glob.glob(glob_pattern)
     if fnames:
         return fnames[0]
 
-def get_telnc_file(obsnum, basepath='/data_lmt/ifproc'):
-    fnames = glob.glob(os.path.join(basepath, "ifproc_*_%d_*.nc" % obsnum))
+def get_telnc_file(obsnum, basepath='/data_lmt/ifproc',
+                   most_recent=True):
+    glob_pattern = os.path.join(basepath, "ifproc_*_%d_*.nc" % obsnum)
+    if most_recent:
+        return most_recent_file(glob_pattern)
+    fnames = glob.glob(glob_pattern)
     if fnames:
         return fnames[0]
 
