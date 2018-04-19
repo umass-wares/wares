fa883fa9efb1ea144b48629202f6f89ff37fae78
Gopal Narayanan <gopastro@gmail.com>
2018-04-19 05:07:50 -0400

    Add area

--- wares/combined/wares_ifproc.py
+++ wares/combined/wares_ifproc.py
@@ -90,7 +90,7 @@ class SpectrumIFProc():
                 self.spectra[inp, :] = self.spectra[inp, :] + numpy.polyval(p, self.velocities)
                               
     def get_area(self, windows=[(-25, 25)]):
-        if not getattr(self, 'sigma'):
+        if not hasattr(self, 'sigma'):
             print "First obtain baseline"
             return
         for i, win in enumerate(windows):
